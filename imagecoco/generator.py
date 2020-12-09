a9904d82e9e70427ba255c1952a30212fd4e8df9
Peter Michael <petermic@cs.washington.edu>
2020-12-09 02:56:20 -0800

    so close to train!

--- imagecoco/generator.py
+++ imagecoco/generator.py
@@ -7,27 +7,40 @@ from torch.distributions import Categorical
 import torch.nn.functional as F
 
 class Generator():
-        def __init__(self, seq_len, vocab_size, token_map):
+        def __init__(self, seq_len, vocab_size, token_map, str_map=None):
                 self.seq_len = seq_len
                 self.vocab_size = vocab_size
 
                 # declare our model, wanting to see hidden states
                 self.model = GPT2LMHeadModel.from_pretrained('gpt2', output_hidden_states=True, use_cache=True)
+
+                # freeze transformer
+                for param in self.model.transformer.parameters():
+                    param.requires_grad = False
+
                 # mod head for our coco vocab
                 self.model.lm_head = nn.Linear(self.model.lm_head.in_features, vocab_size)
 
+                # Just making sure the FC layer is not frozen :)
+                for param in self.model.lm_head.parameters():
+                    param.requires_grad = True
+
+                # Use the same tok
                 self.tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 
                 # due to changing the architecture, we need to map our argmax to the token
                 # in the gpt2 tokenizer.
                 self.token_map = torch.tensor(token_map)
 
+                # map to map non-gpt vocab back into strings
+                self.str_map = np.array(str_map)
+
         def generate(self, batch_size, num_batches, inc_hidden_state, inc_probs, decode):
             # put into eval mode
             self.model.eval()
 
             # placeholder for generated words
-            generated = torch.empty(batch_size*num_batches, self.seq_len)
+            generated = torch.empty(batch_size*num_batches, self.seq_len, dtype=torch.long)
 
             # tensor of probabilities
             probs = torch.empty(batch_size*num_batches, self.seq_len, self.vocab_size)
@@ -41,31 +54,33 @@ class Generator():
 
             # generate sequence
             for i in range(self.seq_len):
+                # forward pass + extract data
                 res = self.model(input_ids=tok, past_key_values=past)
-                prob, past, h_state = res[0], res[1], res[2]
+                prob, past, h_state = res[0], res[1], res[2][-1]
+                
+                # pick out most recent token (if inputted > 1 token)
+                if len(prob.shape) == 3:
+                    prob = prob[:,-1,:]
+                    h_state = h_state[:,-1,:]
 
                 # Attach hidden state (last layer)
-                h_states[:, i, :] = h_state[-1].squeeze(1)
+                h_states[:, i, :] = h_state.squeeze(1)
 
-                # Get dist over tokens (softmax so sum to 1)
-                prob = F.softmax(prob, dim=1)
                 # concat to probs array
-                probs[:, i, :] = prob
+                probs[:, i, :] = F.log_softmax(prob, dim=1)
 
                 # Sample this prob dist for each sentence.
-                dist = Categorical(prob)
-                # Map this to the GPT2 token set.
-                tok = self.token_map[dist.sample()]
+                dist = Categorical(F.softmax(prob, dim=1))
 
-                # Add the new word to all the sentences
-                generated[:, i] = tok
+                # Add the new word to all the sentences (in non-gpt vocab)
+                generated[:, i] = dist.sample()
 
+                # map to gpt2 vocab
+                tok = self.token_map[generated[:, i]]
+
+            # decode=put back to string
             if decode:
-                str_gen = []
-                for s in generated:
-                    str_gen.append(self.tokenizer.decode(s))
-
-                generated=str_gen
+                generated = self.str_map[generated.flatten()].reshape(batch_size*num_batches, self.seq_len)
             else:
                 generated = np.split(np.array(generated), batch_size, axis=0)
 
@@ -80,16 +95,25 @@ class Generator():
         # Gets hidden state for inputted data (for rewards)
         def get_hidden_state(self, data):
             self.model.eval()
-            pass
+
+            # tokenize data
+            tok = self.tokenizer(data)
+            # pass thru transformer
+            h_state = self.model(input_ids=tok)[2][-1]
+
+            return h_state
 
         # fine tune new FC layer  using normal transformer opt & train data
         # https://huggingface.co/transformers/custom_datasets.html
         def pretrain(self, train_path):
 
+            # Get train data
             train_data = LineByLineTextDataset(tokenizer=self.tokenizer, file_path=train_path, block_size=self.seq_len)
 
+            # put model in train mode
             self.model.train()
 
+            # train hyperparams
             training_args = TrainingArguments(
                 output_dir='./results',          # output directory
                 num_train_epochs=3,              # total number of training epochs
@@ -100,6 +124,7 @@ class Generator():
                 logging_steps=10,
             )
 
+            # define trainer
             trainer = Trainer(
                 model=self.model,                         # the instantiated 🤗 Transformers model to be trained
                 args=training_args,                  # training arguments, defined above
