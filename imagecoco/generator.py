95de569b01dec16d81fa430e4e3946cdccc44546
Peter Michael <petermic@cs.washington.edu>
2020-12-13 14:21:06 -0800

    fixes

--- imagecoco/generator.py
+++ imagecoco/generator.py
@@ -80,8 +80,13 @@ class Generator():
                 generated[:, i] = dist.sample()
 
                 # map to gpt2 vocab
-                print(self.str_map[generated[:, :i+1].cpu()].tolist())
-                gpt_map = self.tokenizer(self.str_map[generated[:, :i+1].cpu()].tolist(), padding=True)
+                str_map = self.str_map[generated[:, :i+1].cpu()]
+                if max([len(x) for x in str_map]) == 1:
+                    str_map = np.array(str_map.tolist()).squeeze(1).tolist()
+                else:
+                    str_map = str_map.tolist()
+
+                gpt_map = self.tokenizer(str_map, padding=True)
                 tok =  torch.tensor(gpt_map['input_ids']).cuda()
                 attn_mask = torch.tensor(gpt_map['attention_mask']).cuda()
                 tok_mask = torch.cat((torch.arange(batch_size*num_batches).unsqueeze(1).cuda(), attn_mask.argmax(1).unsqueeze(1)), dim=1)
--- imagecoco/map_token.py
+++ imagecoco/map_token.py
@@ -9,6 +9,7 @@ _, voc = pickle.load(open('save/vocab_cotra.pkl', 'rb'))
 voc_map = dict() # do not include space.
 
 gpt_map = []
+str_map = []
 
 i = 0
 for word in voc.keys():
@@ -17,12 +18,15 @@ for word in voc.keys():
         voc_map[word] = i
         i += 1
         gpt_map.append(tk.encode(word))
+        str_map.append(word) # reverse map
         #voc_map.append(tokenizer.encode(word))
     else:
         print(word)
 
-voc_map['eos'] = i #<eos>
+voc_map[tk.eos_token] = i #<eos>
+str_map.append(tk.eos_token)
 gpt_map.append(50256)
 
 pickle.dump(gpt_map, open('save/gpt_map.pkl', 'wb'))
 pickle.dump(voc_map, open('save/str_vocab_map.pkl', 'wb'))
+pickle.dump(str_map, open('save/str_map.pkl', 'wb'))
