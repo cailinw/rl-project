0709295e02d70b53b650ed90e0150d79e63138aa
Peter Michael <petermic@cs.washington.edu>
2020-12-13 17:54:07 -0800

    fixes

--- imagecoco/test_gen.py
+++ imagecoco/test_gen.py
@@ -6,4 +6,4 @@ str_map = pickle.load(open('save/str_map.pkl', 'rb'))
 
 gen = Generator(10,str_map)
 
-print(gen.generate(2,1,torch.arange(0,16).view(2,2,4), False,False,False))
+print(gen.generate(2,1,torch.arange(0,16).view(2,8), False,False,False))
