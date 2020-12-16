0967d8d6490b823ce2c25e4a79ee9d0246c30da7
cailinw <cailinwin@gmail.com>
2020-12-15 19:24:19 -0800

    blue score???????????????

---
+++ imagecoco/bleu_score.py
@@ -0,0 +1,26 @@
+import sys
+from nltk.translate.bleu_score import corpus_bleu
+
+def main(candidate):
+	references = []
+	with open("/save/str_real_data.txt") as fin:
+		for line in fin:
+            line = line.split()
+            references.append(line)
+
+	candidates = []
+	with open(candidate_file)as fin:
+        for line in fin:
+            line = line.split()
+            candidates.append(line)
+
+    scores = []
+    for candidate in candidates:
+    	score = sentence_bleu(referencess, candidate)
+    	scores.append(score)
+
+    print("BLEU Score: ", sum(scores) / len(scores))
+
+
+if __name__ == "__main__":
+	main(sys.argv[1])
