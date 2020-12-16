404c87dfed4a381a42c7d4f5fc8b952da08679f8
cailinw <cailinwin@gmail.com>
2020-12-15 20:25:47 -0800

    bleu score similar to paper irl text gen implementation??

--- imagecoco/bleu_score.py
+++ imagecoco/bleu_score.py
@@ -1,12 +1,16 @@
+import random
 import sys
-from nltk.translate.bleu_score import corpus_bleu
+from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
 
-def main(candidate):
+def main(candidate_file):
 	references = []
-	with open("/save/str_real_data.txt") as fin:
+	with open("save/str_real_data.txt") as fin:
 		for line in fin:
 			line = line.split()
 			references.append(line)
+	random.shuffle(references)
+	references = references[:1000]
+	
 
 	candidates = []
 	with open(candidate_file)as fin:
@@ -14,12 +18,19 @@ def main(candidate):
 			line = line.split()
 			candidates.append(line)
 
-	scores = []
-	for candidate in candidates:
-		score = sentence_bleu(referencess, candidate)
-		scores.append(score)
+	chencherry = SmoothingFunction()
 
-	print("BLEU Score: ", sum(scores) / len(scores))
+	weights = [(1, 0, 0, 0), (0.5, 0.5, 0, 0), (0.33, 0.33, 0.33, 0), (0.25, 0.25, 0.25, 0.25)]
+	
+	#for i in range(len(weights)):
+		#weight = weights[i]
+	for ngram in range(2, 6):
+		weight = tuple((1. / ngram for _ in range(ngram)))
+		scores = []
+		for candidate in candidates:
+			score = sentence_bleu(references, candidate, weights=weight, smoothing_function=chencherry.method1)
+			scores.append(score)
+		print(f"{len(weight)}-gram: ", sum(scores) / len(scores))
 
 
 if __name__ == "__main__":
